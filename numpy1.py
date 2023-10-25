c5657b4f67153279feb1cb344eabc6c09deab37a
kushagra-gulati <kushagra.gulati@zemosolabs.com>
2023-10-25 16:42:49 +0530

    Revert "initial commit"
    This reverts commit 55ffc1ad1f2b919e71432b108b0bfc44176bc34d.

--- numpy1.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-# Let's create and cast a list of list to generate 2-D array
-my_matrix = [[1,2,3],[4,5,6],[7,8,9]]
-print( my_matrix )
-# Two-dimensional array from Matrix
-matrix_one = np.array(my_matrix) # type: ignore
-print(matrix_one)
