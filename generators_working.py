89cf92207a594338662de8f592c27ab52b281de8
kushagra-gulati <kushagra.gulati@zemosolabs.com>
2023-11-02 17:15:57 +0530

    changes in file generators

--- generators_working.py
+++ generators_working.py
@@ -10,3 +10,16 @@ print(next(gen))
 print(next(gen)) 
 print(next(gen)) 
 print(next(gen)) 
+
+#2nd code
+def doubler_generator():
+    number = 2
+    while True:
+        yield number
+        number *= number
+
+doubler = doubler_generator()
+print (next(doubler))
+print (next(doubler))
+print (next(doubler))
+print (type(doubler))
